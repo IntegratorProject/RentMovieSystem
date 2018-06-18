0abe6b430779e9833380419dc54258400dd2f105
eduar <eduar@DESKTOP-01E6LG3.local>
2018-06-18 17:01:17 -0300

    finalizacao da implementacao do dao generico

--- src/java/dao/GenericDao.java
+++ src/java/dao/GenericDao.java
@@ -2,10 +2,10 @@
 package dao;
 
 import banco.Fabrica;
-import entidade.Cliente;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
 public class GenericDao<T> {
     
@@ -47,6 +47,128 @@ public class GenericDao<T> {
                 manager.close();
             }
             
+        }
+        
+    }
+    
+    public T editar(T object){
+        
+        EntityManager manager = null;
+        EntityTransaction transac = null;
+        
+        try{
+            
+            manager = Fabrica.get().createEntityManager();
+            transac = manager.getTransaction();
+            
+            transac.begin();
+            manager.merge(object);
+            transac.commit();
+            
+        }catch(Exception e){
+            
+            if(transac != null){
+                transac.rollback();
+            }
+            e.printStackTrace();
+            
+        }finally{
+            
+            if(manager != null){
+                manager.close();
+            }
+            
+        }
+        
+        return object;
+        
+    }
+    
+    public boolean delete(long id){
+        
+        EntityManager manager = null;
+        EntityTransaction transc = null;
+        
+        try{
+            
+            manager = Fabrica.get().createEntityManager();
+            T object = manager.find(classe, id);
+            
+            if(object != null){
+                
+                transc = manager.getTransaction();
+                transc.begin();
+                
+                manager.remove(object);
+                
+                transc.commit();
+                
+                return true;
+                
+            }else{
+                return false;
+            }
+            
+        }catch(Exception e){
+            
+            if(transc != null){
+                transc.rollback();
+            }
+            e.printStackTrace();
+            
+            return false;
+            
+        }finally{
+            
+            if(manager != null){
+                manager.close();
+            }
+            
+        }
+        
+    }
+    
+    public T buscarId(long id){
+        
+        EntityManager manager = null;
+        
+        try{
+        
+            manager = Fabrica.get().createEntityManager();
+            return manager.find(classe, id);
+            
+        }catch(Exception e){
+            
+            e.printStackTrace();
+            return null;
+            
+        }finally{
+            if(manager != null){
+                manager.close();
+            }
+        }
+        
+    }
+    
+    public List<T> buscarTodos(){
+        
+        EntityManager manager = null;
+        
+        try{
+            
+            manager = Fabrica.get().createEntityManager();
+            Query query = manager.createQuery(" from "+classe.getName());
+            return query.getResultList();
+            
+        }catch(Exception e){
+            
+            e.printStackTrace();
+            return null;
+            
+        }finally{
+            if(manager != null){
+                manager.close();
+            }
         }
         
     }
