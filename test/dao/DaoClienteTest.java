ee5bbbacbdc9f10376e98bfb746aa925fa34de2c
eduar <eduar@DESKTOP-01E6LG3.local>
2018-06-18 17:01:00 -0300

    teste do dao generico

--- test/dao/DaoClienteTest.java
+++ test/dao/DaoClienteTest.java
@@ -7,12 +7,20 @@ public class DaoClienteTest {
     
     public static void main(String[] args){
         
+        /*
         Cliente c = new Cliente();
-        c.setNome("Eduardo");
+        c.setId(1);
+        c.setNome("Eduaweefw");
         c.setEmail("eryeskurblrsh");
-        
+        */
         GenericDao<Cliente> dao = new GenericDao<>(Cliente.class);
-        dao.salvar(c);
+        
+        for(Cliente c : dao.buscarTodos()){
+            
+            System.out.println(c.getNome());
+            
+        }
+        
         
     }
     
