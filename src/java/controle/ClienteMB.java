5a1e0d553a31ea153414ef3ca085191af027b76d
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2018-08-04 18:10:12 -0300

    implementacao de metodos de cadastro

--- src/java/controle/ClienteMB.java
+++ src/java/controle/ClienteMB.java
@@ -5,22 +5,75 @@ import dao.GenericDao;
 import entidade.Cliente;
 import java.util.ArrayList;
 import java.util.List;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import util.ValidadorCPF;
 
 @ManagedBean
 @ViewScoped
-public class ClienteMB {
+public class ClienteMB extends DefaultMB{
     
     private GenericDao<Cliente> dao = new GenericDao<>(Cliente.class);
     private Cliente cliente = new Cliente();
     private List<Cliente> clientes = new ArrayList<>();
 
     public ClienteMB() {
+        updateList();
+    }
+    
+    public void cadastrar(){
+        
+        ValidadorCPF vCPF = new ValidadorCPF();
+        cliente.setCpf(vCPF.removeMask(cliente.getCpf()));
+        
+        if(vCPF.isCPF(cliente.getCpf())){
+            
+            if(cliente.getId() == 0){
+                
+                try{
+                    
+                    dao.salvar(cliente);
+                    cliente = new Cliente();
+                    updateList();
+                    
+                }catch(Exception e){
+                    connetionError();
+                }
+                
+            }else{
+                
+                try{
+                    
+                    dao.editar(cliente);
+                    cliente = new Cliente();
+                    updateList();
+                    
+                }catch(Exception e){
+                    connetionError();
+                }
+                
+            }
+            
+        }else{
+            showErrorMessage("Erro!", "CPF inválido!");
+        }
+        
     }
     
     public void excluir(long id){
         
+    }
+    
+    private void updateList(){
+        
+        try{
+            clientes = dao.buscarTodos();
+        }catch(Exception e){
+            connetionError();
+        }
+       
     }
     
     public Cliente getCliente() {
