e6dae960a18e937754545d12baa74eeca5e9f86a
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2018-08-18 17:40:33 -0300

    implementacao metodo JQuery para identificar a pagina selecionada e destacar no menu lateral

--- web/resources/algaworks/javascripts/app.js
+++ web/resources/algaworks/javascripts/app.js
@@ -1,6 +1,31 @@
+$(document).ready(function(){
+   
+    var indice = window.location.href.substr(window.location.href.lastIndexOf("/") + 1);
+    
+    $('.selectMenu').removeClass('is-selected');
+    
+    if(indice === "cliente.xhtml"){
+        $('#selectMenuCliente').addClass('is-selected');
+    }
+    
+    if(indice === "dependente.xhtml"){
+        $('#selectMenuDependente').addClass('is-selected');
+    }
+    
+    if(indice === "funcionario.xhtml"){
+        $('#selectMenuFuncionario').addClass('is-selected');
+    }
+    
+    if(indice === "acervo.xhtml"){
+        $('#selectMenuAcervo').addClass('is-selected');
+    }
+    
+});
+
 $(function() {
 	$('.js-toggle').bind('click', function(event) {
 		$('.js-sidebar, .js-content').toggleClass('is-toggled');
 		event.preventDefault();
 	});	
 });
+
