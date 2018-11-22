fcc35ce5b635c044d892229a46d9a7c9195b3e7e
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2018-11-22 14:41:13 -0200

    implementacao de funcao do jquery pra esconder as divs nao selecionadas

--- web/resources/algaworks/javascripts/relatorio.js
+++ web/resources/algaworks/javascripts/relatorio.js
@@ -1,11 +1,6 @@
-function Mudarestado(el) {
-    var display = document.getElementById(el).style.display;
+function Mudarestado(el){
     
+    $('.hd_div').hide();
+    $('#'+el).show();
     
-    
-    if (display === "none")
-        document.getElementById(el).style.display = 'block';
-        
-    else
-        document.getElementById(el).style.display = 'none';
 }
