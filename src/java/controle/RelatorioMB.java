aa4723d6f12c668cc392a89dfa00198573097ccf
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2018-11-23 18:21:50 -0200

    implementando atributos para trabalhar com os campos do relatorio de faturamento por periodo

--- src/java/controle/RelatorioMB.java
+++ src/java/controle/RelatorioMB.java
@@ -1,6 +1,7 @@
 
 package controle;
 
+import java.util.Date;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -8,10 +9,26 @@ import javax.faces.bean.ViewScoped;
 @ViewScoped
 public class RelatorioMB extends DefaultMB{
     
+    private Date relFaturamentoPeriodoInicio;
+    private Date relFaturamentoPeriodoFinal = new Date();
+    
     public void imprimirRelatorio(){
-        
     }
-   
+
+    public Date getRelFaturamentoPeriodoInicio() {
+        return relFaturamentoPeriodoInicio;
+    }
+
+    public void setRelFaturamentoPeriodoInicio(Date relFaturamentoPeriodoInicio) {
+        this.relFaturamentoPeriodoInicio = relFaturamentoPeriodoInicio;
+    }
+
+    public Date getRelFaturamentoPeriodoFinal() {
+        return relFaturamentoPeriodoFinal;
+    }
+
+    public void setRelFaturamentoPeriodoFinal(Date relFaturamentoPeriodoFinal) {
+        this.relFaturamentoPeriodoFinal = relFaturamentoPeriodoFinal;
+    }
+    
 }
-
-
