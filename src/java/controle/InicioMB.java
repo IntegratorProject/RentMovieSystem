271a1db553da83f5179d669a53ab5747153ededb
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2018-11-23 02:31:24 -0200

    implementacao de funcionalidades da tela de inicio

--- src/java/controle/InicioMB.java
+++ src/java/controle/InicioMB.java
@@ -1,40 +1,120 @@
-
 package controle;
 
-import javax.annotation.PostConstruct;
+import dao.GenericDao;
+import dao.MidiaDao;
+import entidade.Cliente;
+import entidade.Locacao;
+import entidade.Midia;
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import org.primefaces.model.DashboardColumn;
-import org.primefaces.model.DashboardModel;
-import org.primefaces.model.DefaultDashboardColumn;
-import org.primefaces.model.DefaultDashboardModel;
 
 @ManagedBean
 @ViewScoped
-public class InicioMB {
-    
-    private DashboardModel model;
-    
-    @PostConstruct
-    public void init(){
-        
-        model = new DefaultDashboardModel();
-        DashboardColumn column1 = new DefaultDashboardColumn();
-        DashboardColumn column2 = new DefaultDashboardColumn();
-         
-        column1.addWidget("locacoesAtrazadas");
-        column1.addWidget("ultimasMovimentacoes");
-         
-        column2.addWidget("faturamentoDiario");
-        column2.addWidget("devolucoesPrevistas");
- 
-        model.addColumn(column1);
-        model.addColumn(column2);
+public class InicioMB implements Serializable {
+
+    private int clientesAtivos = 0;
+    private int locacoesCorrentes = 0;
+    private double faturamentoDiário = 0.d;
+    private int midiasDestruidas = 0;
+
+    private String queryAbleTodayDate = "";
+
+    public InicioMB() {
+
+        Calendar hoje = Calendar.getInstance();
+        queryAbleTodayDate = new SimpleDateFormat("yyyy-MM-dd").format(hoje.getTime());
+
+        updateClientesAtivos();
+        updateLocacoesCorrentes();
+        updateFaturamentaDiario();
+        updateMidiasDestruidas();
         
     }
-    
-    public DashboardModel getModel(){
-        return model;
+
+    private void updateClientesAtivos() {
+        try {
+            GenericDao<Cliente> dao = new GenericDao<>(Cliente.class);
+            List<Cliente> list = dao.buscarCondicao("enable = 1");
+            clientesAtivos = list.size();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateLocacoesCorrentes() {
+        try {
+            GenericDao<Locacao> dao = new GenericDao<>(Locacao.class);
+            List<Locacao> list = dao.buscarCondicao("dataDevolucao = null and dataLocacao >= '" + queryAbleTodayDate + "'");
+            locacoesCorrentes = list.size();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateFaturamentaDiario() {
+        try {
+            GenericDao<Locacao> dao = new GenericDao<>(Locacao.class);
+            List<Locacao> list = dao.buscarCondicao("dataDevolucao = '"+queryAbleTodayDate+"'");
+            
+            double somatoria = 0.d;
+            
+            for(Locacao l : list){
+                somatoria += l.getValorPago();
+            }
+            
+            faturamentoDiário = somatoria;
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
+    private void updateMidiasDestruidas(){
+        try{
+            
+            MidiaDao dao = new MidiaDao();
+            List<Midia> list = dao.buscarCondicao("disponibilidade = 'indisponivel'");
+            midiasDestruidas = list.size();
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public int getClientesAtivos() {
+        return clientesAtivos;
+    }
+
+    public void setClientesAtivos(int clientesAtivos) {
+        this.clientesAtivos = clientesAtivos;
+    }
+
+    public int getLocacoesCorrentes() {
+        return locacoesCorrentes;
+    }
+
+    public void setLocacoesCorrentes(int locacoesCorrentes) {
+        this.locacoesCorrentes = locacoesCorrentes;
+    }
+
+    public double getFaturamentoDiário() {
+        return faturamentoDiário;
+    }
+
+    public void setFaturamentoDiário(double faturamentoDiário) {
+        this.faturamentoDiário = faturamentoDiário;
+    }
+
+    public int getMidiasDestruidas() {
+        return midiasDestruidas;
+    }
+
+    public void setMidiasDestruidas(int midiasDestruidas) {
+        this.midiasDestruidas = midiasDestruidas;
+    }
+
 }
